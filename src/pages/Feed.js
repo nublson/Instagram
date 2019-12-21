7159df20997ca1e7266aea53bf207fc78fc3df76
nubelsondev <nubelsondev@hotmail.com>
2019-12-21 22:30:53 +0000

    feat: add Feed page structure

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,7 +1,110 @@
 import React from "react";
 
+import "./Feed.css";
+
+import more from "../assets/more.svg";
+import like from "../assets/like.svg";
+import comment from "../assets/comment.svg";
+import send from "../assets/send.svg";
+
 const Feed = () => {
-    return <h1>Feed</h1>;
+    return (
+        <section id="post-list">
+            <article>
+                <header>
+                    <div className="user-info">
+                        <span>Nubelson Fernandes</span>
+                        <span className="place">Porto</span>
+                    </div>
+
+                    <img src={more} alt="Mais" />
+                </header>
+                <img
+                    src="http://localhost:3333/files/roman-synkevych-vXInUOv1n84-unsplash.jpg"
+                    alt="Post"
+                />
+
+                <footer>
+                    <div className="actions">
+                        <img src={like} alt="Like" />
+                        <img src={comment} alt="Comment" />
+                        <img src={send} alt="Send" />
+                    </div>
+
+                    <strong>900 likes</strong>
+                    <p>
+                        Roadmap for a All Developer!{" "}
+                        <span>
+                            #self-taugth #buildupdevs #selflearning
+                            #fullstackdeveloper
+                        </span>
+                    </p>
+                </footer>
+            </article>
+            <article>
+                <header>
+                    <div className="user-info">
+                        <span>Nubelson Fernandes</span>
+                        <span className="place">Porto</span>
+                    </div>
+
+                    <img src={more} alt="Mais" />
+                </header>
+                <img
+                    src="http://localhost:3333/files/roman-synkevych-vXInUOv1n84-unsplash.jpg"
+                    alt="Post"
+                />
+
+                <footer>
+                    <div className="actions">
+                        <img src={like} alt="Like" />
+                        <img src={comment} alt="Comment" />
+                        <img src={send} alt="Send" />
+                    </div>
+
+                    <strong>900 likes</strong>
+                    <p>
+                        Roadmap for a All Developer!{" "}
+                        <span>
+                            #self-taugth #buildupdevs #selflearning
+                            #fullstackdeveloper
+                        </span>
+                    </p>
+                </footer>
+            </article>
+            <article>
+                <header>
+                    <div className="user-info">
+                        <span>Nubelson Fernandes</span>
+                        <span className="place">Porto</span>
+                    </div>
+
+                    <img src={more} alt="Mais" />
+                </header>
+                <img
+                    src="http://localhost:3333/files/roman-synkevych-vXInUOv1n84-unsplash.jpg"
+                    alt="Post"
+                />
+
+                <footer>
+                    <div className="actions">
+                        <img src={like} alt="Like" />
+                        <img src={comment} alt="Comment" />
+                        <img src={send} alt="Send" />
+                    </div>
+
+                    <strong>900 likes</strong>
+                    <p>
+                        Roadmap for a All Developer!{" "}
+                        <span>
+                            #self-taugth #buildupdevs #selflearning
+                            #fullstackdeveloper
+                        </span>
+                    </p>
+                </footer>
+            </article>
+        </section>
+    );
 };
 
 export default Feed;
